bf436694df622a177a97299ffc56e537677e7311
turtelneck <65428036+birlumbus@users.noreply.github.com>
2025-02-20 01:22:27 -0600

    Darcy letter dialogue extracted

---
+++ training_data/dacy_letter_dialogue_extractor.py
@@ -0,0 +1,20 @@
+import re
+
+
+def separate_sentences(input, output):
+    with open(input, 'r') as file:
+        text = file.read()
+    
+    # splits at periods, question marks, and exclamation marks followed by spaces
+    sentences = re.split(r'(?<=\.|\?|!)\s+', text)
+    
+    # tag each line
+    with open(output, 'w') as file:
+        for sentence in sentences:
+            file.write(f"<darcy-dialogue> {sentence.strip()}\n")
+
+
+input = '/Users/rhodri/Projects/Darcy/training_data/raw_text/darcy_letter.txt'
+output = '/Users/rhodri/Projects/Darcy/training_data/raw_text/darcy_letter_as_dialogue.txt'
+
+separate_sentences(input, output)
