777b39871985ea9e27c53ea604f78206dc7752e5
turtelneck <65428036+birlumbus@users.noreply.github.com>
2025-03-05 17:05:52 -0600

    Rebuild labeled_training_data_1

--- training_data/data_to_json.py
+++ training_data/data_to_json.py
@@ -2,20 +2,42 @@ import json
 import re
 
 
-input_file = "./training_text/labeled_training_data_2.txt"
-output_file = "./training_text/labeled_training_data_2.json"
+in_1 = "./training_text/labeled_training_data_1.txt"
+out_1 = "./training_text/labeled_training_data_1.json"
+in_2 = "./training_text/labeled_training_data_2.txt"
+out_2 = "./training_text/labeled_training_data_2.json"
 
 structured_data = []
 pattern = re.compile(r"<(.*?)>\s*(.*)")
 
 
-with open(input_file, "r", encoding="utf-8") as f:
-    for line in f:
-        match = pattern.match(line.strip())
-        if match:
-            category, content = match.groups()
-            structured_data.append({"category": category, "content": content})
+def data_to_json(in_file, out_file):
+    # input
+    with open(input_file, "r", encoding="utf-8") as f:
+        # process
+        for line in f:
+            match = pattern.match(line.strip())
+            if match:
+                category, content = match.groups()
+                structured_data.append({"category": category, "content": content})
+    
+    #output
+    with open(output_file, "w", encoding="utf-8") as f:
+        json.dump(structured_data, f, ensure_ascii=False, indent=4)
+        
+
+def main():
+    in_1 = "./training_text/labeled_training_data_2.txt"
+    out_1 = "./training_text/labeled_training_data_2.json"
+    in_2 = "./training_text/labeled_training_data_2.txt"
+    out_2 = "./training_text/labeled_training_data_2.json"
+
+    structured_data = []
+    pattern = re.compile(r"<(.*?)>\s*(.*)")
+    
+    data_to_json(in_1, out_1)
+    data_to_json(in_2, out_2)
 
 
-with open(output_file, "w", encoding="utf-8") as f:
-    json.dump(structured_data, f, ensure_ascii=False, indent=4)
+if __name__ == "__main__":
+    main()
