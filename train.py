619f333700162252e08b42d28e72f10f135fcf86
turtelneck <65428036+birlumbus@users.noreply.github.com>
2025-03-04 22:18:21 -0600

    Update train.py
    forgot to change training params back to 2 epochs

--- train.py
+++ train.py
@@ -14,88 +14,73 @@ def load_data(json_file):
         formatted_samples.append(formatted_text)
     return formatted_samples
 
-formatted_samples = load_data("./training_data/training_text/labeled_training_data_2.json")
 
-print(formatted_samples[0])
-print(formatted_samples[1])
-print(formatted_samples[2])
-print(formatted_samples[3])
-print(formatted_samples[4])
-print(formatted_samples[5])
-print(formatted_samples[6])
-print(formatted_samples[7])
-print(formatted_samples[8])
-print(formatted_samples[9])
+# create DarcyDataset class
+class DarcyDataset(Dataset):
+    def __init__(self, texts, tokenizer, block_size=512):
+        self.examples = []
+        for text in texts:
+            tokenized = tokenizer(
+                text,
+                add_special_tokens=True,
+                truncation=True,
+                max_length=block_size,
+                padding="max_length",
+            )
+            self.examples.append(tokenized["input_ids"])
+    
+    def __len__(self):
+        return len(self.examples)
+    
+    def __getitem__(self, idx):
+        # in language modeling, input ids and labels are the same
+        input_ids = self.examples[idx]
+        return {"input_ids": input_ids, "labels": input_ids}
 
 
-# 
-# 
-# # create DarcyDataset class
-# class DarcyDataset(Dataset):
-#     def __init__(self, texts, tokenizer, block_size=512):
-#         self.examples = []
-#         for text in texts:
-#             tokenized = tokenizer(
-#                 text,
-#                 add_special_tokens=True,
-#                 truncation=True,
-#                 max_length=block_size,
-#                 padding="max_length",
-#             )
-#             self.examples.append(tokenized["input_ids"])
-#     
-#     def __len__(self):
-#         return len(self.examples)
-#     
-#     def __getitem__(self, idx):
-#         # in language modeling, input ids and labels are the same
-#         input_ids = self.examples[idx]
-#         return {"input_ids": input_ids, "labels": input_ids}
-# 
-# 
-# tokenizer = GPT2TokenizerFast.from_pretrained("gpt2-medium")
-# tokenizer.pad_token = tokenizer.eos_token
-# # hoping special tokens will improve tag recognition during training
-# special_tokens_dict = {'additional_special_tokens': ["[CATEGORY:", "[TEXT:"]}
-# num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
-# 
-# # load GPT-2, resize token embeddings
-# model = GPT2LMHeadModel.from_pretrained("gpt2-medium")
-# model.resize_token_embeddings(len(tokenizer))
-# 
-# # instantiate dataset
-# formatted_texts = load_data("./training_data/training_text/labeled_training_data.json")
-# dataset = DarcyDataset(formatted_texts, tokenizer)
-# 
-# # # data collator for dynamic input padding
-# # data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
-# 
-# 
-# # build trainer
-# training_args = TrainingArguments(
-#     output_dir="./model/darcy-gpt",
-#     overwrite_output_dir=True,
-#     num_train_epochs=1,
-#     per_device_train_batch_size=2,
-#     learning_rate=1e-5,
-#     save_steps=500,
-#     save_total_limit=2,
-#     prediction_loss_only=True,
-# )
-# 
-# trainer = Trainer(
-#     model=model,
-#     args=training_args,
-#     train_dataset=dataset,
-# #     data_collator=data_collator,
-# )
-# 
-# 
-# # train!
-# trainer.train()
-# 
-# # explicitly save model
-# trainer.save_model("./model/darcy-gpt")
-# # also save tokenizer, because I've made changes to it
-# tokenizer.save_pretrained("./model/darcy-gpt")
-# 
+tokenizer = GPT2TokenizerFast.from_pretrained("gpt2-medium")
+tokenizer.pad_token = tokenizer.eos_token
+# hoping special tokens will improve tag recognition during training
+special_tokens_dict = {'additional_special_tokens': ["[CATEGORY:", "[TEXT:"]}
+num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
+
+# load GPT-2, resize token embeddings
+model = GPT2LMHeadModel.from_pretrained("gpt2-medium")
+model.resize_token_embeddings(len(tokenizer))
+
+# instantiate dataset
+formatted_texts = load_data("./training_data/training_text/labeled_training_data.json")
+dataset = DarcyDataset(formatted_texts, tokenizer)
+
+# # data collator for dynamic input padding
+# data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+
+# build trainer
+training_args = TrainingArguments(
+    output_dir="./model/darcy-gpt",
+    overwrite_output_dir=True,
+    num_train_epochs=2,
+    per_device_train_batch_size=2,
+    learning_rate=1e-5,
+    save_steps=500,
+    save_total_limit=2,
+    prediction_loss_only=True,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=dataset,
+#     data_collator=data_collator,
+)
+
+
+# train!
+trainer.train()
+
+# explicitly save model
+trainer.save_model("./model/darcy-gpt")
+# also save tokenizer, because I've made changes to it
+tokenizer.save_pretrained("./model/darcy-gpt")
+
