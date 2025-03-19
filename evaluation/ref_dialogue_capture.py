fee17799a79259e3f7004c067ddf171bd8aceb6f
turtelneck <65428036+birlumbus@users.noreply.github.com>
2025-03-19 17:03:49 -0500

    Update ref_dialogue_capture.py
    works as intended now, loading dialogue and only selecting lines of a certain length to save in a new file.

--- evaluation/ref_dialogue_capture.py
+++ evaluation/ref_dialogue_capture.py
@@ -1,32 +1,32 @@
-import random
-
-
-# load dialogues from your file
-def load_dialogues(filepath):
+# load dialogues from file
+def load_dialogue(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
         lines = file.readlines()
     # strip tags and whitespace
-    dialogues = [line.replace('<darcy-dialogue>', '').strip() for line in lines]
-    return dialogues
+    dialogue = [line.replace('<darcy-dialogue>', '').strip() for line in lines]
+    return dialogue
 
 
 # select dialogue of specified length
-def capture_dialogues(dialogues, min_words=8, max_words=20):
+def capture_references(dialogues, min_words=15, max_words=80):
     suitable_references = [d for d in dialogues if min_words <= len(d.split()) <= max_words]
-    return random.sample(suitable_references)
+    return suitable_references
 
 
 # save dialogue to new file
-def save_references
+def save_references(filepath, selected_samples):
+    content = "\n".join(selected_samples)
+    with open(filepath, 'w', encoding='utf-8') as file:
+        file.write(content)
 
 
 if __name__ == '__main__':
-    all_dialogue_txt = '../training_data/training_text/isolated_categories/darcy_dialogue_only.txt'
-    references_txt = '../training_data/training_text/isolated_categories/references_for_analysis.txt'
-    dialogues = load_dialogues(filepath)
-    
-    selected_samples = capture_dialogues(dialogues, min_words=8, max_words=20)
+    all_dialogue = '../training_data/training_text/isolated_categories/darcy_dialogue_only.txt'
+    references = './training_data/training_text/isolated_categories/references_for_analysis.txt'
+
+    dialogue = load_dialogue(all_dialogue)
+    selected_samples = capture_references(dialogue, min_words=8, max_words=20)
 
     print('\nSaving references')
-    save_references(references_txt)
+    save_references(references, selected_samples)
     print('...done.\n')
